7cda6227d52ddb01c1ee833ac49f10ab96ba3569
marechal <marchalzogoung@yahoo.com>
2025-08-12 11:54:30 -0400

    valider_les_données

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,15 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BlogFilterRequest;
 use Illuminate\Http\Request;
 use Illuminate\Pagination\Paginator;
 use \App\Models\Post;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class BlogController extends Controller
 {
     //Creer des fonctions qui correspondent à nos differentes actions
-    public function index(){
+    public function index(BlogFilterRequest $request){
+
+        // $validator = Validator::make(["title"=> "ddfdggfffgd", 'content' => ''], ["title"=> 'min:8']);
+        // dd($validator->validated()); // les autres methodes sont errors, fails etc...
+
+        // Les règles d'un même champ peuvent être definies sous forme de tableau ou en utilisant la classe Rule et ses methodes
+        //les validations se font dans BlogFilterRequest
+        dd($request->validated());
         $posts = Post::paginate(1);
         return view('blog.index', ['posts'=> $posts]);
     }
